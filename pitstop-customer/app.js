7d747f2111c424755a0a6951dd9f32e6ac9b5c63
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2024-06-06 18:23:13 +0100

    added monitoring config files

--- pitstop-customer/app.js
+++ pitstop-customer/app.js
@@ -2,12 +2,46 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require("cors");
+var cors = require('cors');
+var client = require('prom-client');
 
-//routes
+// routes
 var customersRouter = require('./routes/customers');
 
 var app = express();
+
+// Prometheus metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+// Create a Registry to register the metrics
+const register = new client.Registry();
+register.setDefaultLabels({
+  app: 'customers-service'
+});
+
+// Register the default metrics
+client.collectDefaultMetrics({ register });
+
+// Create a custom metric
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code']
+});
+
+register.registerMetric(httpRequestDurationMicroseconds);
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    httpRequestDurationMicroseconds
+      .labels(req.method, req.route ? req.route.path : req.path, res.statusCode)
+      .observe(duration);
+  });
+  next();
+});
 
 // CORS middleware for all routes
 app.use(cors());
@@ -20,5 +54,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/api/customers', customersRouter);
- 
+
+// Expose metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 module.exports = app;

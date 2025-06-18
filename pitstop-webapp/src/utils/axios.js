7e87306de525834a92757b815504ef6ddf97a7d8
salmmalop <sas829991@gmail.com>
2025-06-18 17:28:16 +0100

    Update axios.js

--- pitstop-webapp/src/utils/axios.js
+++ pitstop-webapp/src/utils/axios.js
@@ -1,32 +1,11 @@
-import Axios from "axios"
+import Axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 let API_GATEWAY = Axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: `${BASE_URL}/api`
 });
 
-// let CustomerAPI = Axios.create({
-//     baseURL: "http://localhost:8080/api/customers/"
-// });
-// let VehicleAPI = Axios.create({
-//     baseURL: "http://localhost:3002/api/vehicles/"
-// });
-
-// let WorkshopAPI = Axios.create({
-//     baseURL: "http://localhost:3003/api/workshop/"
-// });
-// WorkshopAPI.interceptors.request.use(async (req) => {
-//     let available = await store.dispatch("WORKSHOP_SERVICE_AVAILABILITY");
-//     if (available) {
-//         return req;
-//     }
-//     return req;
-// }, (error) => {
-//     return Promise.reject(error);
-// });
 export {
-    API_GATEWAY,
-    // CustomerAPI,
-    // VehicleAPI,
-    // WorkshopAPI
-    
-}
+    API_GATEWAY
+};
